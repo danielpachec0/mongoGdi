8560ee2574dafb1b2aae26d452b954517b78e6fc
Daniel Pacheco <daniel.azepac@outlook.com>
2021-08-20 15:08:58 -0300

    New querrys for mongosh

--- src/queries.js
+++ src/queries.js
@@ -1,57 +1,7 @@
-// const {MongoClient} = require('mongodb');
-
-
-// async function listDatabases(client){
-//     databasesList = await client.db().admin().listDatabases();
- 
-//     console.log("Databases:");
-//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
-
-// async function main(){
-//     const uri = "mongodb+srv://projetomongo:123321@cluster0.yyvzu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
- 
-//     const client = new MongoClient(uri);
- 
-//     try {
-//         await client.connect();
-        
-//         // await  listDatabases(client);
-        
-//         // await client.db("joguitos").collection("jogador").find({nome: "daniel"}).toArray()
-//         // .then(items => {
-//         //   console.log(`Successfully found ${items.length} documents.`)
-//         //   console.log(items[0].nome)
-//         //   return items
-//         // })
-//         // .catch(err => console.error(`Failed to find documents: ${err}`))
-
-//         // console.log(await client.db("joguitos").collection("jogador").countDocuments());
-
-//         // console.log(await client.db("joguitos").collection("jogador").countDocuments({nome: "daniel"}));
-        
-//         // await client.db("joguitos").collection("jogador").find().toArray()
-//         // .then(items => {
-//         //   console.log(`Successfully found ${items.length} documents.`)
-//         //   console.log(items)
-//         //   return items
-//         // })
-//         // .catch(err => console.error(`Failed to find documents: ${err}`));
-
-//         client.db("joguitos").collection("jogador").find()
-
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         await client.close();
-//     }
-    
-// }
-
-// main().catch(console.error);
-
-//db = db.getSiblingDB("joguitos");
+db = db.getSiblingDB("joguitos");
 console.log(db.jogador.find());
 console.log(db.jogador.find({nome: "Art"}).pretty());
 console.log(db.time.count());
-db
+db.campeonato.updateOne({nome: "teste"},{$set:{"nome": "IEM"}})
+console.log(db.time.find());
+console.log(db.time.find().sort({vitorias: -1}));

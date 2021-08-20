d0e2d84e2b44b6b0b2418c7f02fa40a6998aa0e2
gustavogaldino <gcgm@cin.ufpe.br>
2021-08-20 15:42:49 -0300

    Added aggregate with match

--- src/queries.js
+++ src/queries.js
@@ -1,7 +1,18 @@
+const { Timestamp } = require("mongodb");
+
 db = db.getSiblingDB("joguitos");
-console.log(db.jogador.find());
+/*console.log(db.jogador.find());
 console.log(db.jogador.find({nome: "Art"}).pretty());
 console.log(db.time.count());
 db.campeonato.updateOne({nome: "teste"},{$set:{"nome": "IEM"}})
 console.log(db.time.find());
-console.log(db.time.find().sort({vitorias: -1}));
+console.log(db.time.find().sort({vitorias: -1}));*/
+
+var startDate = new Date("Tue Aug 20 2021 15:26:12 GMT-0300 (Brasilia Standard Time)").getTime();
+console.log(db.campeonato.aggregate([
+    {
+        $match: {
+            dt_inicio: {$lt: startDate}
+        },
+    }
+]));

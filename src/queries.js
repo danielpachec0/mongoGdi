22c05f8dfe3e7c9a06d2cddf2d9f96223bcc9c5b
Daniel Pacheco <daniel.azepac@outlook.com>
2021-08-20 15:45:52 -0300

    add queries

--- src/queries.js
+++ src/queries.js
@@ -1,13 +1,16 @@
 const { Timestamp } = require("mongodb");
 
-db = db.getSiblingDB("joguitos");
-/*console.log(db.jogador.find());
+
+console.log(db.jogador.find());
+console.log(db.time.findOne({pais: "Brasil"}));
 console.log(db.jogador.find({nome: "Art"}).pretty());
 console.log(db.time.count());
 db.campeonato.updateOne({nome: "teste"},{$set:{"nome": "IEM"}})
 console.log(db.time.find());
-console.log(db.time.find().sort({vitorias: -1}));*/
-
+console.log(db.time.find().sort({vitorias: -1}));
+db.jogador.renameCollection("proplayer");
+save() --> deprecated
+console.log(db.time.aggregate({$match:{pais: "Brasil"}}, {$max: "vitorias"}));
 var startDate = new Date("Tue Aug 20 2021 15:26:12 GMT-0300 (Brasilia Standard Time)").getTime();
 console.log(db.campeonato.aggregate([
     {

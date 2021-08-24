66fd1aece3500955e94795e300852cd9de9ec72d
daniel <dap5@cin.ufpe.br>
2021-08-23 23:32:04 -0300

    commenting the code

--- populate.js
+++ populate.js
@@ -1,3 +1,7 @@
+//insert many que adciona varios objetos
+//do tipo jogadores para o banco de dados
+db.dropDatabase()
+
 db.jogador.insertMany([{
     nome: "Art",
     pais: "Brasil",
@@ -216,7 +220,8 @@ db.jogador.insertMany([{
     CPF: "30"
 
 }])
-
+//Inseri um objeto time com nome furia que faz referencia
+//a 5 jogaroes que ja estão no banco de daods 
 db.time.insertOne(
     {
         nome: "Furia",
@@ -224,14 +229,16 @@ db.time.insertOne(
         participacoes: 10,
         vitorias: 5,
         jogadores: [
-            db.jogador.find({nome: "Art"})._id,
-            db.jogador.find({nome: "Yuri"})._id,
-            db.jogador.find({nome: "Vini"})._id,
-            db.jogador.find({nome: "Kacerato"})._id,
-            db.jogador.find({nome: "Honda"})._id
+            db.jogador.findOne({nome: "Art"})._id,
+            db.jogador.findOne({nome: "Yuri"})._id,
+            db.jogador.findOne({nome: "Vini"})._id,
+            db.jogador.findOne({nome: "Kacerato"})._id,
+            db.jogador.findOne({nome: "Honda"})._id
         ]
     }
 )
+//Inseri um objeto time com nome Godsent que faz referencia
+//a 5 jogaroes que ja estão no banco de daods
 db.time.insertOne(
     {
         nome: "Godsent",
@@ -239,14 +246,16 @@ db.time.insertOne(
         participacoes: 12,
         vitorias: 3,
         jogadores: [
-            db.jogador.find({nome: "TACO"})._id,
-            db.jogador.find({nome: "felps"})._id,
-            db.jogador.find({nome: "latto"})._id,
-            db.jogador.find({nome: "bartin"})._id,
-            db.jogador.find({nome: "dumau"})._id
+            db.jogador.findOne({nome: "TACO"})._id,
+            db.jogador.findOne({nome: "felps"})._id,
+            db.jogador.findOne({nome: "latto"})._id,
+            db.jogador.findOne({nome: "bartin"})._id,
+            db.jogador.findOne({nome: "dumau"})._id
         ]
     }
 )
+//Inseri um objeto time com nome Pain Gaming que faz referencia
+//a 5 jogaroes que ja estão no banco de dados
 db.time.insertOne(
     {
         nome: "Pain Gaming",
@@ -254,14 +263,16 @@ db.time.insertOne(
         participacoes: 6,
         vitorias: 2,
         jogadores: [
-            db.jogador.find({nome: "cAmyy"})._id,
-            db.jogador.find({nome: "izaa"})._id,
-            db.jogador.find({nome: "pan"})._id,
-            db.jogador.find({nome: "anna"})._id,
-            db.jogador.find({nome: "bizinha"})._id
+            db.jogador.findOne({nome: "cAmyy"})._id,
+            db.jogador.findOne({nome: "izaa"})._id,
+            db.jogador.findOne({nome: "pan"})._id,
+            db.jogador.findOne({nome: "anna"})._id,
+            db.jogador.findOne({nome: "bizinha"})._id
         ]
     }
 )
+//Inseri um objeto time com nome O plano que faz referencia
+//a 5 jogaroes que ja estão no banco de dados
 db.time.insertOne(
     {
         nome: "O Plano",
@@ -269,15 +280,17 @@ db.time.insertOne(
         participacoes: 7,
         vitorias: 5,
         jogadores: [
-            db.jogador.find({nome: "vsm"})._id,
-            db.jogador.find({nome: "leo_drk"})._id,
-            db.jogador.find({nome: "kNgV-"})._id,
-            db.jogador.find({nome: "trk"})._id,
-            db.jogador.find({nome: "LUCAS1"})._id,
-            db.jogador.find({nome: "HEN1"})._id
+            db.jogador.findOne({nome: "vsm"})._id,
+            db.jogador.findOne({nome: "leo_drk"})._id,
+            db.jogador.findOne({nome: "kNgV-"})._id,
+            db.jogador.findOne({nome: "trk"})._id,
+            db.jogador.findOne({nome: "LUCAS1"})._id,
+            db.jogador.findOne({nome: "HEN1"})._id
         ]
     }
 )
+//Inseri um objeto time com nome "Team Liquid" que faz referencia
+//a 5 jogaroes que ja estão no banco de dados
 db.time.insertOne(
     {
         nome: "Team Liquid",
@@ -285,14 +298,16 @@ db.time.insertOne(
         participacoes: 17,
         vitorias: 7,
         jogadores: [
-            db.jogador.find({nome: "Fallen"})._id,
-            db.jogador.find({nome: "Elige"})._id,
-            db.jogador.find({nome: "Stewie"})._id,
-            db.jogador.find({nome: "Grim"})._id,
-            db.jogador.find({nome: "Naf"})._id
+            db.jogador.findOne({nome: "Fallen"})._id,
+            db.jogador.findOne({nome: "Elige"})._id,
+            db.jogador.findOne({nome: "Stewie"})._id,
+            db.jogador.findOne({nome: "Grim"})._id,
+            db.jogador.findOne({nome: "Naf"})._id
         ]
     }
 )
+//Inseri um objeto time com nome "Rainha da bala" que faz referencia
+//a 5 jogaroes que ja estão no banco de dados
 db.time.insertOne(
     {
         nome: "Rainhas da bala",
@@ -300,14 +315,16 @@ db.time.insertOne(
         participacoes: 17,
         vitorias: 7,
         jogadores: [
-            db.jogador.find({nome: "gabi"})._id,
-            db.jogador.find({nome: "rebeca"})._id,
-            db.jogador.find({nome: "lethy"})._id,
-            db.jogador.find({nome: "naty"})._id,
-            db.jogador.find({nome: "elaine"})._id
+            db.jogador.findOne({nome: "gabi"})._id,
+            db.jogador.findOne({nome: "rebeca"})._id,
+            db.jogador.findOne({nome: "lethy"})._id,
+            db.jogador.findOne({nome: "naty"})._id,
+            db.jogador.findOne({nome: "elaine"})._id
         ]
     }
 )
+//Inseri um objeto campeonato com nome "teste" que faz referencia
+//a 2 times que ja estão no banco de dados
 db.campeonato.insertOne(
     {
         Nome: "teste",
@@ -319,11 +336,13 @@ db.campeonato.insertOne(
         ],
         premiacao: 1000.00,
         times: [
-            db.time.find({nome: "Team Liquid"})._id,
-            db.time.find({nome: "Furia"})._id
+            db.time.findOne({nome: "Team Liquid"})._id,
+            db.time.findOne({nome: "Furia"})._id
         ]
     }
 )
+//Inseri um objeto campeonato com nome "MAJOR 2021" que faz referencia
+//a 3 times que ja estão no banco de dados
 db.campeonato.insertOne(
     {
         Nome: "MAJOR 2021",
@@ -335,12 +354,14 @@ db.campeonato.insertOne(
         ],
         premiacao: 1000000.00,
         times: [
-            db.time.find({nome: "Godsent"})._id,
-            db.time.find({nome: "O Plano"})._id,
-            db.time.find({nome: "Furia"})._id
+            db.time.findOne({nome: "Godsent"})._id,
+            db.time.findOne({nome: "O Plano"})._id,
+            db.time.findOne({nome: "Furia"})._id
         ]
     }
 )
+//Inseri um objeto campeonato com nome "Campeonato TPM" que faz referencia
+//a 4 times que ja estão no banco de dados
 db.campeonato.insertOne(
     {
         Nome: "Campeonato TPM",
@@ -351,16 +372,18 @@ db.campeonato.insertOne(
             {nome: "j2", cpf: "321", idade: 42}
         ],
         times: [
-            db.time.find({nome: "Team Liquid"})._id,
-            db.time.find({nome: "Furia"})._id,
-            db.time.find({nome: "O Plano"})._id,
-            db.time.find({nome: "Godsent"})._id
+            db.time.findOne({nome: "Team Liquid"})._id,
+            db.time.findOne({nome: "Furia"})._id,
+            db.time.findOne({nome: "O Plano"})._id,
+            db.time.findOne({nome: "Godsent"})._id
         ]
     }
 )
+//Inseri um objeto campeonato com nome "Pro league" que faz referencia
+//a 3 times que ja estão no banco de dados
 db.campeonato.insertOne(
     {
-        Nome: "teste",
+        Nome: "Pro league",
         dt_inicio: (Date.now() - 86400000),
         dt_termino: Date.now(),
         juizes:[
@@ -369,12 +392,14 @@ db.campeonato.insertOne(
         ],
         premiacao: 10000.00,
         times: [
-            db.time.find({nome: "Godsent"})._id,
-            db.time.find({nome: "Furia"})._id,
-            db.time.find({nome: "Team Liquid"})._id
+            db.time.findOne({nome: "Godsent"})._id,
+            db.time.findOne({nome: "Furia"})._id,
+            db.time.findOne({nome: "Team Liquid"})._id
         ]
     }
 )
+//Inseri um objeto campeonato com nome "Campeonato Brasileiro CSGO 2019"
+//que faz referencia a 3 times que ja estão no banco de dados
 db.campeonato.insertOne(
     {
         Nome: "Campeonato Brasileiro CSGO 2019",
@@ -386,12 +411,14 @@ db.campeonato.insertOne(
         ],
         premiacao: 1000.00,
         times: [
-            db.time.find({nome: "Pain Gaming"})._id,
-            db.time.find({nome: "Furia"})._id,
-            db.time.find({nome: "O Plano"})._id
+            db.time.findOne({nome: "Pain Gaming"})._id,
+            db.time.findOne({nome: "Furia"})._id,
+            db.time.findOne({nome: "O Plano"})._id
         ]
     }
 )
+//Inseri um objeto campeonato com nome "Campeonato Brasileiro Feminino 2020"
+//que faz referencia a 2 times que ja estão no banco de dados
 db.campeonato.insertOne(
     {
         Nome: "Campeonato Brasileiro Feminino 2020",
@@ -403,8 +430,8 @@ db.campeonato.insertOne(
         ],
         premiacao: 1000.00,
         times: [
-            db.time.find({nome: "Pain Gaming"})._id,
-            db.time.find({nome: "Rainhas da bala"})._id
+            db.time.findOne({nome: "Pain Gaming"})._id,
+            db.time.findOne({nome: "Rainhas da bala"})._id
         ]
     }
 );
--- queries.js
+++ queries.js
@@ -1,13 +1,29 @@
+//Retorna todos os jogadores inseridos no banco de dados.
 console.log(db.jogador.find());
+
+//Retorna todos os times com país de origem brasil
 console.log(db.time.findOne({pais: "Brasil"}));
+
+//retorna o jogador com nome "Art"
 console.log(db.jogador.find({nome: "Art"}).pretty());
+
+//retorna o numero de times registrados no banco de dados
 console.log(db.time.count());
+
+//faz um update de um campeonato que tenha o nome igual a "teste" e muda o seu nome para "IEM" 
 db.campeonato.updateOne({nome: "teste"},{$set:{"nome": "IEM"}})
+
+//retorna os times registrados no bd e depois retorna eles com um sort
+//para ordenar do time com mais vitorias para o com menos
 console.log(db.time.find());
 console.log(db.time.find().sort({vitorias: -1}));
+
+//aqui mudamos o nome da coleção "jogador" parra "proplayer"
 db.jogador.renameCollection("proplayer");
-save() --> deprecated
-console.log(db.time.aggregate({$match:{pais: "Brasil"}}, {$max: "vitorias"}));
+
+
+//Lista todos os campeonatos que tem data de inicio a
+//data da variavel start Date
 var startDate = new Date("Tue Aug 20 2021 15:26:12 GMT-0300 (Brasilia Standard Time)").getTime();
 console.log(db.campeonato.aggregate([
     {
@@ -17,7 +33,8 @@ console.log(db.campeonato.aggregate([
     }
 ]));
 
-
+//consulta que retorna o total agredo de
+//vitorias dos paises com times registrados
 console.log(db.time.aggregate([
     {
         $group: {
@@ -30,15 +47,17 @@ console.log(db.time.aggregate([
     }
 ]))
 
+//retorna todas os jogadores  com idade maio que 18
 console.log(
-    db.jogador.find(
+    db.proplayer.find(
         {
             idade: {$gte: 18}
         }
     )
 )
 
-console.log(db.jogador.aggregate([
+//printa 5 joigadores e quais os times do pais dele
+console.log(db.proplayer.aggregate([
     {
     $lookup: {
         from: "time",
@@ -50,6 +69,8 @@ console.log(db.jogador.aggregate([
   {$limit: 5}
 ]));
 
+//usa a funcao add to set para adcionar os times passados para todos os
+//campeonatos que tem o nome "teste"
 db.campeonato.updateMany({Nome: "teste"},{$addToSet: 
     {
         times:
@@ -67,9 +88,14 @@ db.campeonato.updateMany({Nome: "teste"},{$addToSet:
     }
 })
 
+//retorna o time com nome pain gaming depois faz uma consulta igual mas usando
+//projection de forma implicita para n mostar o id nem os jogadores
 console.log(db.time.findOne({nome: "Pain Gaming"}))
-console.log(db.time.findOne({nome: "Pain Gaming"}, {jogadores: 0, _id: 0}))
+console.log(db.time.findOne({nome: "Pain Gaming"}, {proplayer: 0, _id: 0}))
+//console.log(db.time.findOne({nome: "Pain Gaming"}, {proplayer: 0, _id: 0}).pretty())
 
+//cassifica os times em 'Top tier' e 'f' 
+//top tier se o numero de vitorias for maior que 5 
 console.log(db.time.aggregate(
     [
        {
@@ -86,6 +112,8 @@ console.log(db.time.aggregate(
     ]
  ));
 
+//Retorna campeonatos que tem um array de times que contem todos os que 
+//foram passqados na consulta
 console.log(db.campeonato.find({times: {$all: [
     db.time.findOne({nome: "Furia"})._id,
     db.time.findOne({nome: "Godsent"})._id,
@@ -95,24 +123,9 @@ console.log(db.campeonato.find({times: {$all: [
     db.time.findOne({nome: "Rainhas da bala"})._id,
 ]}}))
 
-db.time.aggregate( [
-   { $addFields:
-        {
-         message:
-            { $function:
-                {
-                    body: function(nome, vitorias) {
-                        let total = Array.sum(vitorias);
-                        return `Hello ${nome}.  Your total score is ${total}.`
-                    },
-                    args: [ "$nome", "$vitorias"],
-                    lang: "js"
-                }
-            }
-        }
-    }
-])
 
+
+//retorna osjuizes de cada campeonato que tem idade maior que 40 
 console.log(db.campeonato.aggregate([
     {
        $project: {
@@ -129,25 +142,71 @@ console.log(db.campeonato.aggregate([
     }
  ]));
 
+//Retorna os campeonatos que tem "Brasileiro" no nome
 db.campeonato.createIndex( { Nome:"text"} )
 console.log(db.campeonato.find( { $text: { $search: "\"Brasileiro\"" } } ));
 
-//campeonatos com 4 jogadores
+//Retorna os campeonatos com 4 jogadores
 console.log(db.campeonato.find({times:{$size: 4}}));
-//times com 1 jogador reserva
-console.log(db.time.find({jogadores:{$size: 6}}));
 
+//Retorna os Times que tem 6 jogadores
+console.log(db.time.find({proplayer:{$size: 6}}));
+
+//Retorna apenas os campeonatos que tem uma premiacao
 console.log(db.campeonato.find( { premiacao: { $exists: true}}, {_id:0, juizes: 0, times: 0}));
 
+//consulta que retonar o pais que possui o mair numero de
+//vitorios dos que tem  pais de origem brasil
+console.log(db.time.aggregate({$match:{pais: "Brasil"}}, {$max: "vitorias"}));
+
+//retorna a media de idade de jogadores com pais de origem brasil
+console.log(db.proplayer.aggregate(
+    [
+      {
+        $group:
+          {
+            _id: "$pais",
+            avgQuantity: { $avg: "$idade" }
+          }
+      }
+    ]
+))
+
+//Retorna os campeonatos que tem o array de time igual ao array passado na
+//funcao com o metodo where
+console.log(db.campeonato.find({$where: function() { return this.nome == [
+    db.time.findOne({nome: "Godsent"})._id,
+    db.time.findOne({nome: "Furia"})._id,
+    db.time.findOne({nome: "Team Liquid"})._id
+] }}));
+
+//
+console.log(db.time.aggregate( [
+    { $addFields:
+         {
+          message:
+             { $function:
+                 {
+                     body: function(nome, vitorias) {
+                         let total = Array.sum(vitorias);
+                         return `o time ${nome}tem um total de vitorias de: ${total}.`
+                     },
+                     args: [ "$nome", "$vitorias"],
+                     lang: "js"
+                 }
+             }
+         }
+     }
+ ]));
+ 
 //map reduce esta como depracatedclea
+//save() tbm
 var mapFunction1 = function() {
     emit(this.funcao, this.idade);
 };
-
 var reduceFunction1 = function(keyfuncao, idades) {
     return Array.sum(idades);
 };
-
 db.jogador.mapReduce(
     mapFunction1,
     reduceFunction1,
